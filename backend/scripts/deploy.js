20c5bd6077c1d4ec6152b2cb111bbe6890cdf282
AmirHossein Mobayen <a.h.mobayen@gmail.com>
2022-11-12 16:04:43 +0100

    update and deployed - ui connection is not working

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -7,20 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const twitterContractFactory = await ethers.getContractFactory('TwitterContract');
+  const twitterContract = await twitterContractFactory.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  await twitterContract.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Contract deployed to: ", twitterContract.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -29,25 +21,3 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
-
-// main function from: https://javascript.plainenglish.io/build-a-twitter-clone-using-react-and-solidity-8aa8a1715186
-// must check and replaced on previous code. much cleaner
-const main = async() => {
-  const twitterContractFactory = await ethers.getContractFactory('TwitterContract');
-  const twitterContract = await twitterContractFactory.deploy();
-  await twitterContract.deployed();
-  console.log("Contract deployed to: ", twitterContract.address);
-};
-
-// duplicate function (must remove)
-const runMain = async() => {
-  try {
-    await main();
-    process.exit(0);
-  } catch(error) {
-    console.log(error);
-    process.exit(1);
-  }
-}
-
-;runMain();
